c67ae8dd06345dab099cf984c2276072235c7dd8
TruongVanPhuc123 <tvphuc2k4@gmail.com>
2024-03-03 23:32:11 +0700

    Counter with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [Count, setCount] = useState(0)
+    let color = Count < 0 ? 'red' : Count > 0 ? 'green' : 'black';
+
+    const increase = () => setCount(Count + 1)
+    const uncrease = () => setCount(Count - 1)
+    const reset = () => setCount(0)
+    return (
+        <div class="container">
+            <h1 class="head">Counter</h1>
+            <div style={{ color }} id="number" class="numberUpDown">{Count}</div>
+            <div class="btnGroup">
+                <button onClick={uncrease} class="btn btnDescrease">Decrease</button>
+                <button onClick={reset} class="btn btnReset">Reset</button>
+                <button onClick={increase} class="btn btnIncrease">Increase</button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
