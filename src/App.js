141bd88bd492122b78805acfcd720566982ae666
TruongVanPhuc123 <tvphuc2k4@gmail.com>
2024-03-03 21:05:34 +0700

    Like and UnLike

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [like, setLike] = useState(0);
+    const [blog, setblog] = useState({
+        content: "React is awesome",
+        count: 0
+    })
+
+    const handleCLick = () => {
+        setLike(1 - like);
+        setblog({ ...blog, count: blog.count + 1 })
+    }
+
+    return (
+        <div style={{ display: 'flex', gap: '30px', flexDirection: 'column' }}>
+            <h1>{blog.content}</h1>
+            <h1 style={{ cursor: "pointer" }} onClick={handleCLick}>{like === 1 ? 'like ' : 'unlike '}{blog.count}</h1>
+            <h1 style={{ color: like === 1 ? "red" : "black" }}>&#9829;</h1>
+        </div>
+    )
+
 }
 
-export default App;
+export default App
