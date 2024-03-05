3bce1257b7a23bec2a10ef85149f64d0741ef4b2
TruongVanPhuc123 <tvphuc2k4@gmail.com>
2024-03-05 21:40:23 +0700

    FinallProject ReactJS Basic

--- src/components/PartList.js
+++ src/components/PartList.js
@@ -4,8 +4,8 @@ export const PartList = ({ total, path, setClick, check, top = "50%", height = "
     let parts = [];
     for (let i = 0; i < total; i++) {
         parts.push(
-            <div key={path + i} className={check == i ? `selected clickable square` : `clickable square`} onClick={() => setClick(i)}>
-                <img src={`./character/${path}/${i}.png`} alt="" className="img-center" style={{ height, top }} />
+            <div key={path + i} className={check === i ? `selected clickable square` : `clickable square`} onClick={() => setClick(i)}>
+                <img src={`./character/${path}/${i}.png`} alt={""} className="img-center" style={{ height, top }} />
             </div>
         )
     }
